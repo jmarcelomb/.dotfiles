217b49c0e8027b668aa484a26f8e89ece87e3747
Marcelo <jmmb1098@gmail.com>
2024-10-20 14:21:11 +0100

    disables bluetooth

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -1,7 +1,7 @@
 {
   hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
+    enable = false; # save energy until we need it
+    powerOnBoot = false;
     settings = {
       General = {
         Enable = "Source,Sink,Media,Socket";
