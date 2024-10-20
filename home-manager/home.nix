2b9fe1dce27450060f9425223d387adb45ed56f9
Marcelo <jmmb1098@gmail.com>
2024-10-20 15:03:16 +0100

    adds scripts to bin

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,5 +8,8 @@
     username = "jmmb";
     homeDirectory = "/home/jmmb";
     stateVersion = "24.05";
+    file.".local/bin".source =
+      config.lib.file.mkOutOfStoreSymlink "${homeDirectory}/.dotfiles/scripts";
+  };
   };
 }
