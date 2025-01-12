a92359bf62e06e00cedbcbf92ffa50b88eec0e9c
Marcelo Borges <jmmb1098@gmail.com>
2025-01-12 22:37:08 +0000

    show current apps in sketchybar & updates when focus changes

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
+# Make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on
+# Get the app names for the given workspace
+WORKSPACE="$1"
+APPS=$(aerospace list-windows --workspace "$WORKSPACE" | awk -F'|' 'NF > 1 {gsub(/^ +| +$/, "", $2); print $2}' | paste -sd ', ' -)
+
+# Update the sketchybar item
+if [ -z "$APPS" ]; then
+  sketchybar --set "$NAME"
 else
-  sketchybar --set $NAME background.drawing=off
+  sketchybar --set "$NAME" label="$WORKSPACE:$APPS"
+fi
+
+# Set background drawing based on focused workspace
+if [ "$WORKSPACE" = "$FOCUSED_WORKSPACE" ]; then
+  sketchybar --set "$NAME" background.drawing=on
+else
+  sketchybar --set "$NAME" background.drawing=off
 fi
