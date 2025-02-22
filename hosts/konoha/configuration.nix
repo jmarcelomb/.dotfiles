1566697c8353d4808b0e37fdff83ee0f33ae5514
Marcelo Borges <git@marceloborges.dev>
2025-02-22 12:40:19 +0000

    chore: update flake.lock, gpg module and vmware shared dir
    Update the pinned versions of home-manager, nixpkgs, and nixpkgs-stable to their latest revisions. This ensures compatibility with the latest features and bug fixes from upstream repositories.
    Add a systemd service to automatically mount VMware shared folders using vmhgfs-fuse. This improves usability for VMware guests by ensuring shared folders are accessible at /mnt/hgfs.
    Introduce a new module for GPG configuration, enabling pcscd and GPG agent with SSH support. This enhances security and simplifies key management for GPG and SSH operations.
    
    Signed-off-by: Marcelo Borges <git@marceloborges.dev>

--- hosts/konoha/configuration.nix
+++ hosts/konoha/configuration.nix
@@ -15,4 +15,21 @@
 
   virtualisation.vmware.guest.enable = true;
 
+  systemd.services.mount-vmhgfs = {
+    description = "Mount VMware Shared Folders";
+    after = [ "network.target" "open-vm-tools.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.open-vm-tools}/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other";
+      ExecStop = "${pkgs.util-linux}/bin/umount /mnt/hgfs";
+      RemainAfterExit = true;
+    };
+  };
+
+  # Ensure the /mnt/hgfs directory exists
+  system.activationScripts.mkHGFSDir = ''
+    mkdir -p /mnt/hgfs
+  '';
 }
