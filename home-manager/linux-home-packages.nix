b966a38f0ce0fc9ccce7700d458430f45baa9fa8
Marcelo Borges <git@marceloborges.dev>
2025-05-01 18:39:17 +0100

    feat(configuration): pass isServer to common home packages import

--- home-manager/linux-home-packages.nix
+++ home-manager/linux-home-packages.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, isServer, ... }:
 
 let
-  common = import ./common-home-packages.nix { inherit pkgs; };
+  common = import ./common-home-packages.nix { inherit pkgs isServer; };
 in {
   home.packages = common.home.packages ++ (with pkgs; [
     wl-clipboard
