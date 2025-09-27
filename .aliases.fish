6a3b2e751ef9dd6f66627b43edeadae21da9656b
Marcelo Borges <marcelo.borges@synopsys.com>
2025-09-27 20:26:10 +0100

    feat(shell): add help function with syntax highlighting for --help output
      Adds a help function that pipes command help output through bat with
      syntax highlighting. Also sets MANPAGER to use bat for colorized man
      pages and fixes path expansion in aliases source statement.
    
    Signed-off-by: Marcelo Borges <marcelo.borges@synopsys.com>

--- .aliases.fish
+++ .aliases.fish
@@ -12,6 +12,12 @@ end
 if type -q bat
     alias cat="bat --style=plain --paging=never"
     alias less='bat --paging=always'
+    alias bathelp='bat --plain --language=help'
+
+    function help
+        "$argv" --help 2>&1 | bathelp
+    end
+    alias h='help'
 end
 
 source ~/.aliases.common
