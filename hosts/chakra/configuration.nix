2058664c8a57b5391552a21cdb9ee93989a083c9
Marcelo Borges <git@marceloborges.dev>
2025-11-09 15:25:15 +0000

    feat(chakra): add nfs client support and mount backup share
    Enable rpcbind service and configure NFS mount point for TrueNAS
    backup storage at /mnt/nas/chakra with NFSv4 options.
    
    Signed-off-by: Marcelo Borges <git@marceloborges.dev>

--- hosts/chakra/configuration.nix
+++ hosts/chakra/configuration.nix
@@ -25,4 +25,14 @@
   networking.hostName = hostname;
   system.stateVersion = stateVersion;
   services.openssh.enable = true;
+
+  # NFS client support
+  services.rpcbind.enable = true;
+  
+  # Mount NFS share from TrueNAS server for backups
+  fileSystems."/mnt/nas/chakra" = {
+    device = "truenas.home:/mnt/nas/chakra";
+    fsType = "nfs";
+    options = [ "nfsvers=4" "rw" "soft" "intr" ];
+  };
 }
