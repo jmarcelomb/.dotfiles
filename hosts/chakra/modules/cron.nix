f711fbff59d098e45622b171257f480d744e4940
Marcelo Borges <git@marceloborges.dev>
2025-11-16 00:36:33 +0000

    feat(cron): add daily docker volumes backup to nas
    Signed-off-by: Marcelo Borges <git@marceloborges.dev>

--- hosts/chakra/modules/cron.nix
+++ hosts/chakra/modules/cron.nix
@@ -5,6 +5,9 @@
     systemCronJobs = [
       # run daily at 20:00 (8:00pm)
       "0 20 * * *   sudo rsync -r --delete ~/server jmmb@192.168.1.124:~/"
+      # Backup Docker volumes to NAS daily at 2:00 AM
+      # to check logs do: journalctl -t docker-backup
+      "0 2 * * *   root   rsync -av --delete /home/hinata/server/volumes/ /mnt/nas/chakra/docker-volumes-backup/ 2>&1 | logger -t docker-backup"
     ];
   };
 }
