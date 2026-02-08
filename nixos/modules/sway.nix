05a70020a6b18250bc4c16fb78f738727c6a524a
Marcelo Borges <git@marceloborges.dev>
2026-02-08 21:58:26 +0000

    fix(sway): remove greetd autologin initial_session for login prompt
      remove initial_session config to disable autologin, ensuring users
      are prompted before entering sway session
    
    Signed-off-by: Marcelo Borges <git@marceloborges.dev>

--- nixos/modules/sway.nix
+++ nixos/modules/sway.nix
@@ -1,17 +1,13 @@
 { pkgs, lib, config, user, ... }:
 
 {
-  # Enable greetd display manager with automatic login
+  # Enable greetd display manager with login prompt
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
         user = "greeter";
-      };
-      initial_session = {
-        command = "sway";
-        inherit user;
       };
     };
   };
