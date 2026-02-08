2836ba022debef53eac47437adbe4bdeeedd465e
Marcelo Borges <git@marceloborges.dev>
2026-02-08 22:30:53 +0000

    feat(theme): set dark color scheme system-wide and update theme variants
      update GTK theme name for Catppuccin Frappe to new variant
      set dconf setting for global dark color scheme preference
      add environment variable to enforce dark theme on GTK apps
      improves theme consistency across applications and system
    
    Signed-off-by: Marcelo Borges <git@marceloborges.dev>

--- home-manager/modules/theme.nix
+++ home-manager/modules/theme.nix
@@ -4,10 +4,10 @@
   # GTK theme configuration - Catppuccin Frappe
   gtk = {
     enable = true;
-    
+
     # Catppuccin Frappe theme
     theme = {
-      name = "Catppuccin-Frappe-Standard-Blue-Dark";
+      name = "catppuccin-frappe-blue-standard+rimless";
       package = pkgs.catppuccin-gtk.override {
         accents = [ "blue" ];
         size = "standard";
@@ -15,22 +15,34 @@
         variant = "frappe";
       };
     };
-    
+
     # Icon theme
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
-    
+
     # Cursor theme
     cursorTheme = {
       name = "catppuccin-frappe-dark-cursors";
       package = pkgs.catppuccin-cursors.frappeDark;
       size = 24;
     };
-    
+
     # Force dark mode for GTK3/4 apps
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
+  };
+
+  # Set dark color scheme via dconf for Firefox and other apps
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  # Environment variables to enforce dark mode
+  home.sessionVariables = {
+    GTK_THEME = "catppuccin-frappe-blue-standard+rimless:dark";
   };
 }
