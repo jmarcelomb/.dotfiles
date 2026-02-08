42e06bfabb2b30377a4bb6733b6e6deb01673a43
Marcelo Borges <git@marceloborges.dev>
2026-02-08 22:02:11 +0000

    feat(sway): add user-menu and remember options to tuigreet command
      Enhance tuigreet invocation by adding --user-menu and --remember flags
      to improve login UX in sway session.
    
    Signed-off-by: Marcelo Borges <git@marceloborges.dev>

--- nixos/modules/sway.nix
+++ nixos/modules/sway.nix
@@ -6,7 +6,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --user-menu --remember --cmd sway";
         user = "greeter";
       };
     };
