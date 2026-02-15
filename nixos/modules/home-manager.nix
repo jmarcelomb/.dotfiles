c8fdf5d1618b521d2f8fe094e43f36d40ff5702c
Marcelo Borges <git@marceloborges.dev>
2026-02-15 17:00:07 +0000

    feat(home-manager): customize backup command to overwrite old backups
      replace default file extension with custom backup command to overwrite
      existing .backup files using mv -f. allows for more controlled backup
      behavior when managing user files with home-manager
    
    Signed-off-by: Marcelo Borges <git@marceloborges.dev>

--- nixos/modules/home-manager.nix
+++ nixos/modules/home-manager.nix
@@ -1,4 +1,9 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [ inputs.home-manager.nixosModules.default ];
-  home-manager.backupFileExtension = "backup";
+
+  # Custom backup command that overwrites old .backup files
+  home-manager.backupFileExtension = null;
+  home-manager.backupCommand = ''
+    ${pkgs.bash}/bin/bash -c 'if [ -e "$1" ]; then ${pkgs.coreutils}/bin/mv -f "$1" "$1.backup"; fi' --
+  '';
 }
